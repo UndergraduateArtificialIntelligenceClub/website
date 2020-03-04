9da309e8d8db4becbfb74324c7aff51b6deee541
giancarlopernudisegura <pernudig@gmail.com>
2020-03-04 00:19:45 -0700

    implemented slickJS

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -2,13 +2,15 @@
 console.log('%cif ur reading this, hi ;)', 'color: lightseagreen; font-size: 24px;');
 
 
-function nextImage() {
-    let imgDirSize = 7;
-    let currImg = document.querySelector('.carousel > img');
-    let loc = currImg.src.split('/');
-    let index = parseInt(loc[loc.length - 1], 10);
-    loc[loc.length - 1] = (index + 1) % imgDirSize + '.jpg';
-    currImg.src = loc.join('/');
-}
-
-setInterval(() => {nextImage()}, 3000);
+$(document).ready(function() {
+    $('.carousel').slick({
+        //dots: true,
+        lazyLoad: 'ondemand',
+        centerMode: true,
+        autoplay: true,
+        infinite: true,
+        centerMode: true,
+        autoplaySpeed: 2000,
+        adaptiveWidth: true
+    });
+});
