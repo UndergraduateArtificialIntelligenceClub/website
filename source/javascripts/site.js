3b9c8de0df32220a16c3bd4722784c1aa7082142
giancarlopernudisegura <pernudig@gmail.com>
2020-03-03 22:33:34 -0700

    basic carousel

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,2 +1,14 @@
 // This is where it all goes :)
 console.log('%cif ur reading this, hi ;)', 'color: lightseagreen; font-size: 24px;');
+
+
+function nextImage() {
+    let imgDirSize = 7;
+    let currImg = document.querySelector('.carousel > img');
+    let loc = currImg.src.split('/');
+    let index = parseInt(loc[loc.length - 1], 10);
+    loc[loc.length - 1] = (index + 1) % imgDirSize + '.jpg';
+    currImg.src = loc.join('/');
+}
+
+setInterval(() => {nextImage()}, 3000);
