b9ba459583f8b3edeafe9a7eb9cc52ba690ed2fb
giancarlopernudisegura <pernudig@gmail.com>
2020-10-01 07:16:15 -0600

    add site description

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import { desc } from './src/assets/data/about.json'
+
 export default {
 	mode: 'spa',
 	srcDir: 'src/',
@@ -7,7 +9,7 @@ export default {
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+			{ hid: 'description', name: 'description', property: 'og:description', content: desc }
 		],
 		link: [
 			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
