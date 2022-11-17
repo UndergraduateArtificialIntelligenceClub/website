c8dae3a676aea30c3fe6062e81067ef3ba2daa0a
Giancarlo Pernudi Segura <pernudig@gmail.com>
2022-11-16 19:33:02 -0500

    create hex tournament page

---
+++ src/pages/hex.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+div
+	section.section
+		h1.title Fall 2022 Hex Tournament
+		h2.subtitle Important Links
+		.content
+			ul
+				li(v-for='main_link in hex_tournament.main_links')
+					a.is-block(:href='main_link.link') {{ main_link.title }}
+</template>
+
+<script >
+export default {
+	head: {
+		title: 'Hex Tournament'
+	},
+	async asyncData({ $content, params }) {
+		const resources = await $content(params.slug || 'resources').fetch()
+		return {
+			hex_tournament: resources.hex_tournament,
+		}
+	}
+}
+</script>
