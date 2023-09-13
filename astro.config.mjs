662889947d44568543578004fceacfe3cac2fd2c
Giancarlo Pernudi Segura <giancarlopernudi@hotmail.com>
2023-09-13 14:45:32 +0000

    upgrade to astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { astroImageTools } from 'astro-imagetools'
 import compress from 'astro-compress'
 import compressor from 'astro-compressor'
 import { defineConfig } from 'astro/config'
-import image from '@astrojs/image'
 import purgecss from 'astro-purgecss'
 import sitemap from "@astrojs/sitemap"
 import vue from '@astrojs/vue'
@@ -10,9 +9,6 @@ import vue from '@astrojs/vue'
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    image({
-      serviceEntryPoint: '@astrojs/image/sharp'
-    }),
     vue(),
     astroImageTools,
     sitemap(),
