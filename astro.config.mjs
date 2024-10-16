799cfec55ed48c4f8b04ee791b03864b921102df
Giancarlo Pernudi Segura <pernudig@gmail.com>
2024-10-16 01:18:45 -0400

    fix katex rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,11 @@ import compress from 'astro-compress'
 import compressor from 'astro-compressor'
 import { defineConfig } from 'astro/config'
 import purgecss from 'astro-purgecss'
-import sitemap from "@astrojs/sitemap"
+import sitemap from '@astrojs/sitemap'
 import vue from '@astrojs/vue'
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,12 +17,12 @@ export default defineConfig({
     sitemap(),
     purgecss({ safelist: [/^.fc/, '#calendar'] }),
     compress(),
-    compressor()
+    compressor(),
+    mdx({
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeKatex],
+    }),
   ],
-  site: "https://uais.dev",
+  site: 'https://uais.dev',
   output: 'static',
-  markdown: {
-		remarkPlugins: [remarkMath],
-		rehypePlugins: [rehypeKatex]
-	}
 });
